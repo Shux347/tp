332ca971fcff24db5b6010c00166c7cf61c77ce1
Varun Shukla <vslpahm@gmail.com>
2024-11-10 19:27:35 +0800

    Change back original error message

--- src/main/java/seedu/internbuddy/logic/parser/ParserUtil.java
+++ src/main/java/seedu/internbuddy/logic/parser/ParserUtil.java
@@ -23,7 +23,7 @@ import seedu.internbuddy.model.tag.Tag;
  */
 public class ParserUtil {
 
-    public static final String MESSAGE_INVALID_INDEX = "Index is not a positive integer.";
+    public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
 
     /**
      * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
